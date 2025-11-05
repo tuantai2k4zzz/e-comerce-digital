3c17232e4230b1a02a6a3569563685b28093e813
Tuan Tai <buituantai2k4@gmail.com>
2025-11-05 18:11:59 +0700

    Change register and create api login and correct password - user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,15 +9,42 @@ const register = asyncHandler(async (req, res) => {
             mes: "Missing inputs"
         });
     }
-    const response = await User.create(req.body)
-    return res.status(200).json({
-        success: true,
-        mes: "Register successfully",
-        user: response,
-    });
+    const user = await User.findOne({email})
+    if(user) {
+        throw new Error('User has existed!')
+    }else {
+      const newUser = await User.create(req.body)
+       return res.status(200).json({
+            success: newUser ? true : false,
+            mes: newUser ?  "Register is succesfully. Please go login!" : "Something went wrong!",
+            user: newUser,
+        });
+    }
     
+})
+
+const login = asyncHandler(async(req, res) => {
+    const {email, password} = req.body
+    
+    if(!email || !password) {
+        return res.status(400).json({
+            success: false,
+            mes: "Missing inputs!"
+        })
+    }
+    const response = await User.findOne({email})
+    if(response && await response.isCorrectPassword(password) === true) {
+        const {password, role, ...userData} = response.toObject()
+        return res.status(200).json({
+            success: true,
+            userData
+        })
+    }else {
+        throw new Error('Invalid credentials!')
+    }
 })
 
 module.exports = {
     register,
+    login,
 }
--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,11 @@ userSchema.pre('save', async function(next) {
     }
     next();
 })
+userSchema.methods = {
+    isCorrectPassword: async function (password) {
+        return await bcrypt.compare(password, this.password)
+    }
+}
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
